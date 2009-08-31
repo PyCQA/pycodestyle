061c39faa6ab7a7cbed9e9037b45fd8a5bfcbf86
reinout <reinout@vanrees.org>
2009-08-31 14:00:58 +0200

    Added setup.py to make it easy_installable.  This allows upload to pypi.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+version = '0.3dev'
+long_description = open('ANNOUNCE').read()
+
+setup(name='pep8',
+      version=version,
+      description="Python style guide checker",
+      long_description=long_description,
+      classifiers=[],
+      keywords='pep8',
+      author='Johann C. Rocholl',
+      author_email='johann@browsershots.org',
+      url='http://github.com/cburroughs/pep8.py/tree/master',
+      license='Expat license',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points={
+          'console_scripts': [
+              'pep8 = pep8:_main',
+              ],
+          },
+      )
