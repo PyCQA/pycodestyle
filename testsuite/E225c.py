7e8583dd20bd4b9df1d5f2eeca4a2b0b7e9284f9
Johann C. Rocholl <johann@rocholl.net>
2009-10-02 13:14:13 -0700

    Added test cases for E225.

---
+++ testsuite/E225c.py
@@ -0,0 +1,1 @@
+hypot2 = x*x + y*y
---
+++ testsuite/E225not.py
@@ -0,0 +1,9 @@
+i = i + 1
+submitted += 1
+x = x * 2 - 1
+hypot2 = x * x + y * y
+c = (a + b) * (a - b)
+foo(bar, key='word', *args, **kwargs)
+baz(**kwargs)
+negative = -1
+spam(-1)
