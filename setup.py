388341031256e7d723030465d73b961582cef33f
reinout <reinout@vanrees.org>
2009-10-20 09:47:21 +0200

    Back to development

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.4'
+version = '0.5dev'
 long_description = '\n\n'.join([open('README.rst').read(),
                                 open('CHANGES.txt').read(),
                                 open('TODO.txt').read()])
