75a1880fd0f04fece3f98d5d14037fab65335032
reinout <reinout@vanrees.org>
2009-10-20 09:45:10 +0200

    preparing 0.4

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.4dev'
+version = '0.4'
 long_description = '\n\n'.join([open('README.rst').read(),
                                 open('CHANGES.txt').read(),
                                 open('TODO.txt').read()])
