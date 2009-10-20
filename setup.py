8330667f66a773af3023e53547e0d0e04904ac06
reinout <reinout@vanrees.org>
2009-10-20 09:39:32 +0200

    README.txt was renamed to README.rst, reflect this in setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 version = '0.4dev'
-long_description = '\n\n'.join([open('README.txt').read(),
+long_description = '\n\n'.join([open('README.rst').read(),
                                 open('CHANGES.txt').read(),
                                 open('TODO.txt').read()])
 
