b7e490bc6076bea278f9f8f3d0e234f5fbfd0475
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-06-18 22:29:23 +0200

    Make sure the file exists before checking it

--- pep8.py
+++ pep8.py
@@ -1581,6 +1581,9 @@ class StyleGuide(object):
 
     def input_file(self, filename, lines=None, expected=None, line_offset=0):
         """Run all checks on a Python source file."""
+        if not os.path.exists(filename):
+            print('No such file found: %s' % filename)
+            return
         if self.options.verbose:
             print('checking %s' % filename)
         fchecker = Checker(filename, lines=lines, options=self.options)
