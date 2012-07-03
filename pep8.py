7416cf94180d4f423ba3404fdc482a06c348f430
Florent Xicluna <florent.xicluna@gmail.com>
2012-07-03 18:07:11 +0200

    Back to development.

--- pep8.py
+++ pep8.py
@@ -93,7 +93,7 @@ for space.
 
 """
 
-__version__ = '1.3.3'
+__version__ = '1.3.4a0'
 
 import os
 import sys
@@ -472,7 +472,7 @@ def continuation_line_indentation(logical_line, tokens, indent_level, verbose):
     # visual indents
     indent = [indent_level]
     indent_chances = {}
-    last_indent = (0, 0)
+    last_indent = tokens[0][2]
     if verbose >= 3:
         print(">>> " + tokens[0][4].rstrip())
 
