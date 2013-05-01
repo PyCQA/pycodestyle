c85fa440d461d4cb715d0ffd8f43675420e5b8a2
Florent Xicluna <florent.xicluna@gmail.com>
2013-05-01 23:57:05 +0200

    Add tests for E265

--- testsuite/E26.py
+++ testsuite/E26.py
@@ -6,8 +6,25 @@ x = x + 1  #Increment x
 x = x + 1  #  Increment x
 #: E262
 x = y + 1  #:  Increment x
+#: E265
+#Block comment
+a = 1
 #: Okay
 pass  # an inline comment
 x = x + 1   # Increment x
 y = y + 1   #: Increment x
-#:
+
+# Block comment
+a = 1
+
+# Block comment1
+
+# Block comment2
+aaa = 1
+
+
+# example of docstring (not parsed)
+def oof():
+    """
+    #foo not parsed
+    """
