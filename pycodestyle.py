c7490b0f3ff1d18fae8c7f12b835885dc3bd19fc
Ian Lee <IanLee1521@gmail.com>
2016-05-31 11:59:34 -0700

    Tagging 2.0.0 alpha 1 pre-release

--- pycodestyle.py
+++ pycodestyle.py
@@ -64,7 +64,7 @@ try:
 except ImportError:
     from ConfigParser import RawConfigParser
 
-__version__ = '2.0.0.dev0'
+__version__ = '2.0.0a1'
 
 DEFAULT_EXCLUDE = '.svn,CVS,.bzr,.hg,.git,__pycache__,.tox'
 DEFAULT_IGNORE = 'E121,E123,E126,E226,E24,E704,W503'
