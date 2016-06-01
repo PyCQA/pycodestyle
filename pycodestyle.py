64edd89c9e487bc202b63ed7e6fc1ff530687f84
Ian Lee <IanLee1521@gmail.com>
2016-05-31 22:19:13 -0700

    Bumped version number post release

--- pycodestyle.py
+++ pycodestyle.py
@@ -64,7 +64,7 @@ try:
 except ImportError:
     from ConfigParser import RawConfigParser
 
-__version__ = '2.0.0'
+__version__ = '2.0.1.dev0'
 
 DEFAULT_EXCLUDE = '.svn,CVS,.bzr,.hg,.git,__pycache__,.tox'
 DEFAULT_IGNORE = 'E121,E123,E126,E226,E24,E704,W503'
