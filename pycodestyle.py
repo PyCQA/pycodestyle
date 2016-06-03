fdf3e6a2655c8ddd465781e1c2693886a8aa762f
Mike Miller <mike@mtmxr.com>
2016-06-03 10:03:24 -0700

    Report E742 and E743 for badly named functions and classes

--- pycodestyle.py
+++ pycodestyle.py
@@ -1163,13 +1163,18 @@ def ambiguous_identifier(logical_line, tokens):
     E741: O = 123
     E741: I = 42
 
-    Variables can be bound in other contexts, so identifiers appearing after
-    the 'as' keyword are also reported:
+    Variables can be bound in several other contexts, including class and
+    function definitions, 'global' and 'nonlocal' statements, exception
+    handlers, and 'with' statements.
 
     Okay: except AttributeError as o:
     Okay: with lock as L:
     E741: except AttributeError as O:
     E741: with lock as l:
+    E741: global I
+    E741: nonlocal l
+    E742: class I(object):
+    E743: def l(x):
     """
     idents_to_avoid = ('l', 'O', 'I')
     prev_type, prev_text, prev_start, prev_end, __ = tokens[0]
@@ -1185,6 +1190,12 @@ def ambiguous_identifier(logical_line, tokens):
             if text in idents_to_avoid:
                 ident = text
                 pos = start
+        if prev_text == 'class':
+            if text in idents_to_avoid:
+                yield start, "E742 ambiguous class definition '%s'" % text
+        if prev_text == 'def':
+            if text in idents_to_avoid:
+                yield start, "E743 ambiguous function definition '%s'" % text
         if ident:
             yield pos, "E741 ambiguous variable name '%s'" % ident
         prev_type = token_type
