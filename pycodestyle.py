d12e2ea39745bd3bb9e819d7342e4c1d73dbb008
Ian Lee <IanLee1521@gmail.com>
2016-11-04 08:59:22 -0700

    Bump versin number post release

--- pycodestyle.py
+++ pycodestyle.py
@@ -66,7 +66,7 @@ try:
 except ImportError:
     from ConfigParser import RawConfigParser
 
-__version__ = '2.1.0'
+__version__ = '2.2.0.dev0'
 
 DEFAULT_EXCLUDE = '.svn,CVS,.bzr,.hg,.git,__pycache__,.tox'
 DEFAULT_IGNORE = 'E121,E123,E126,E226,E24,E704,W503'
