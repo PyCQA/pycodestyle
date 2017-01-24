e4312f75d8e589836e080aeeae28aae0a5c7b087
Anthony Sottile <asottile@umich.edu>
2017-01-24 14:36:04 -0800

    Don't report E701 for variable annotations (#612)

--- testsuite/python3.py
+++ testsuite/python3.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
+from typing import ClassVar, List
 
 
 # Annotated function (Issue #29)
 def foo(x: int) -> int:
     return x + 1
+
+
+# Annotated variables #575
+CONST: int = 42
+
+
+class Class:
+    cls_var: ClassVar[str]
+
+    def m(self):
+        xs: List[int] = []
