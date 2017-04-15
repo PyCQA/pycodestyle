d87a44367c5542ab8052c212e6d51f1532086dd1
Eddie Antonio Santos <easantos@ualberta.ca>
2017-04-15 17:03:49 -0600

    Make identifiers camel-case; remove redundant space.

--- testsuite/python3.py
+++ testsuite/python3.py
@@ -12,6 +12,7 @@ CONST: int = 42
 
 
 class Class:
+    # Camel-caes
     cls_var: ClassVar[str]
     for_var: ClassVar[str]
     while_var: ClassVar[str]
@@ -23,16 +24,16 @@ class Class:
     except_var: ClassVar[str]
     finally_var: ClassVar[str]
     with_var: ClassVar[str]
-    For_var: ClassVar[str]
-    While_var: ClassVar[str]
-    Def_var: ClassVar[str]
-    If_var: ClassVar[str]
-    Elif_var: ClassVar[str]
-    Else_var: ClassVar[str]
-    Try_var: ClassVar[str]
-    Except_var: ClassVar[str]
-    Finally_var: ClassVar[str]
-    With_var: ClassVar[str]
-        
+    forVar: ClassVar[str]
+    whileVar: ClassVar[str]
+    defVar: ClassVar[str]
+    ifVar: ClassVar[str]
+    elifVar: ClassVar[str]
+    elseVar: ClassVar[str]
+    tryVar: ClassVar[str]
+    exceptVar: ClassVar[str]
+    finallyVar: ClassVar[str]
+    withVar: ClassVar[str]
+
     def m(self):
         xs: List[int] = []
