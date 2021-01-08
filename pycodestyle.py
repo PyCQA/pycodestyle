62003f65573947f2620b97c0f8cb79c22ac68b25
Anthony Sottile <asottile@umich.edu>
2021-01-07 17:28:44 -0800

    2.7.0

--- pycodestyle.py
+++ pycodestyle.py
@@ -78,7 +78,7 @@ try:
 except ImportError:
     from ConfigParser import RawConfigParser
 
-__version__ = '2.6.0'
+__version__ = '2.7.0'
 
 DEFAULT_EXCLUDE = '.svn,CVS,.bzr,.hg,.git,__pycache__,.tox'
 DEFAULT_IGNORE = 'E121,E123,E126,E226,E24,E704,W503,W504'
