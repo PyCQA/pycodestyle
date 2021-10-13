7e18554be44a86f95b3070a5d362386ff9efdc8b
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-10-13 15:22:21 +0200

    Additional testcases

--- testing/data/python310.py
+++ testing/data/python310.py
@@ -22,6 +22,10 @@ match var:
         pass
     case (0, 1, *_):
         pass
+    case {"x": 42}:
+        pass
+    case SomeClass(arg=2):
+        pass
 #: E271:2:6 E271:3:9 E271:5:9 E271:7:9
 var = 1
 match  var:
@@ -40,3 +44,20 @@ match(var):
         pass
     case_:
         pass
+#: E271:2:9 E271:3:9 E271:6:9 E271:7:9 E271:8:9
+match var:
+    case  (2, 3): pass
+    case  (
+        2, 3
+    ): pass
+    case  {"x": 42}: pass
+    case  [{"y": 2}, 3]: print()  # comment
+    case  "Hello: World": pass
+#: E221:6:5 E221:7:5
+# Don't emit false-positives
+# Only E221 (multiple spaces before operator), not E271!
+match: int = 42
+case: int = 42
+matched = {"true": True, "false": False}
+case  = {"x": 42}
+case  = "Hello: World"
