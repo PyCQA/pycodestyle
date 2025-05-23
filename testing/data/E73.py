cc602a93ff5f3aca1df34039057bdd4da3ab58ee
mshafer-NI <23644905+mshafer-NI@users.noreply.github.com>
2025-05-23 11:45:08 -0500

    Add test case that shows problem and cases that should still work
    Signed-off-by: mshafer-NI <23644905+mshafer-NI@users.noreply.github.com>

--- testing/data/E73.py
+++ testing/data/E73.py
@@ -1,5 +1,9 @@
 #: E731:1:1
 f = lambda x: 2 * x
+#: E731:1:1
+f = (  # some long comment that forces this to be on a new line
+    lambda x: 731 * x
+)
 #: E731:1:1 E226:1:16
 f = lambda x: 2*x
 #: E731:2:5
@@ -8,6 +12,13 @@ while False:
 #: Okay
 f = object()
 f.method = lambda: 'Method'
+
+f = (lambda o: o.lower()) if isinstance('a', str) else (lambda o: o)
+
+f = (
+    lambda o: o.lower(),
+    lambda o: o.upper(),
+)
 
 f = {}
 f['a'] = lambda x: x ** 2
